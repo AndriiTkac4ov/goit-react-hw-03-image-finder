518a052bb8686c777dd3d2203421278117da2266
AndriiTkac4ov <106906924+AndriiTkac4ov@users.noreply.github.com>
2023-01-09 13:14:55 +0200

    finish Modal in ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -3,20 +3,28 @@ import PropTypes from 'prop-types';
 import { GalleryItem, GalleryItem_image } from "./ImageGalleryItem.styled";
 import { Modal } from "../Modal/Modal";
 
-const showModal = false;
-
 export class ImageGalleryItem extends Component {
+    state = {
+        showModal: false,
+    }
+
+    toggleModal = () => {
+        this.setState(prevState => ({ showModal: !prevState.showModal }));
+    }
+
     render() {
         const { image: { webformatURL, largeImageURL, tags } } = this.props;
+        const { showModal } = this.state;
 
         return (
             <>
-                <GalleryItem>
+                <GalleryItem onClick={this.toggleModal}>
                     <GalleryItem_image src={webformatURL} alt={tags} />
                 </GalleryItem>
                 {showModal && <Modal
                     largeImageURL={largeImageURL}
                     tags={tags}
+                    onCloseModal={this.toggleModal}
                 />}
             </>
         )
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,31 +1,43 @@
-// import { Component } from "react";
+import { Component } from "react";
 import PropTypes from 'prop-types';
 import { Overlay, ModalWindow } from "./Modal.styled";
 
-export const Modal = ({ largeImageURL, tags }) => {
-    return (
-        <Overlay>
-            <ModalWindow>
-                <img src={largeImageURL} alt={tags} />
-            </ModalWindow>
-        </Overlay>
-    )
+export class Modal extends Component {
+    handleCloseByEsc = event => {
+        if (event.code === 'Escape') {
+            this.props.onCloseModal();
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('keydown', this.handleCloseByEsc);
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('keydown', this.handleCloseByEsc);
+    }
+
+    handleCloseByOverlay = (event) => {
+        if (event.target === event.currentTarget) {
+            this.props.onCloseModal();
+        }
+    }
+    
+    render() {
+        const { largeImageURL, tags } = this.props;
+
+        return (
+            <Overlay onClick={this.handleCloseByOverlay}>
+                <ModalWindow>
+                    <img src={largeImageURL} alt={tags} />
+                </ModalWindow>
+            </Overlay>
+        )
+    }
 }
 
 Modal.propTypes = {
     largeImageURL: PropTypes.string.isRequired,
     tags: PropTypes.string.isRequired,
+    onCloseModal: PropTypes.func.isRequired,
 }
-
-// export class Modal extends Component {
-//     render() {
-//         return (
-//             <Overlay>
-//                 <ModalWindow>
-//                     {this.props.children}
-//                     {/* <img src="" alt="" /> */}
-//                 </ModalWindow>
-//             </Overlay>
-//         );
-//     }
-// }
